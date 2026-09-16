60cbd3b795da8e00b8264db76da504c42a61ca55
lahcen404 <lahcen.maskour2003@gmail.com>
2026-09-16 15:59:41 +0100

    add test the risk calculs

--- transformation/risk_score.py
+++ transformation/risk_score.py
@@ -1,4 +1,6 @@
 class RiskCalculator:
+    
+    
 
     def temperature_risk(self, temperature):
 
@@ -17,33 +19,60 @@ class RiskCalculator:
         else:
             return 100
         
-def precipitation_risk(self, precipitation):
+    def precipitation_risk(self, precipitation):
 
-    if precipitation == 0:
-        return 0
+        if precipitation == 0:
+            return 0
 
-    elif precipitation < 5:
-        return 20
+        elif precipitation < 5:
+            return 20
 
-    elif precipitation < 20:
-        return 50
+        elif precipitation < 20:
+            return 50
 
-    elif precipitation < 50:
-        return 75
+        elif precipitation < 50:
+            return 75
 
-    else:
-        return 100
-    
-def wind_risk(self, wind_speed):
+        else:
+            return 100
+        
+    def wind_risk(self, wind_speed):
 
-    if wind_speed < 20:
-        return 0
+        if wind_speed < 20:
+            return 0
 
-    elif wind_speed < 40:
-        return 30
+        elif wind_speed < 40:
+            return 30
 
-    elif wind_speed < 60:
-        return 70
+        elif wind_speed < 60:
+            return 70
 
-    else:
-        return 100
+        else:
+            return 100
+        
+        
+    def calculate_score(self, data):
+
+            data = data.copy()
+
+            data["temperature_risk"] = data["temperature_max"].apply(
+                self.temperature_risk
+            )
+
+            data["precipitation_risk"] = data["precipitation_sum"].apply(
+                self.precipitation_risk
+            )
+
+            data["wind_risk"] = data["wind_speed_10m_max"].apply(
+                self.wind_risk
+            )
+
+            data["risk_score"] = (
+                data["temperature_risk"] * 0.25
+                + data["precipitation_risk"] * 0.40
+                + data["wind_risk"] * 0.35
+            )
+
+            data["risk_score"] = data["risk_score"].round(2)
+
+            return data
---
+++ transformation/test_risk.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from risk_score import RiskCalculator
+
+
+weather = pd.read_csv("data/silver/weather.csv")
+
+calculator = RiskCalculator()
+
+result = calculator.calculate_score(weather)
+
+#print(result.head())
+print(result[
+    [
+        "city",
+        "forecast_date",
+        "temperature_max",
+        "precipitation_sum",
+        "wind_speed_10m_max",
+        "temperature_risk",
+        "precipitation_risk",
+        "wind_risk",
+        "risk_score"
+    ]
+].head(15))
