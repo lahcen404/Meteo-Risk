57fcf22be64f9b3ccd76648192360f04a2aa2122
lahcen404 <lahcen.maskour2003@gmail.com>
2026-09-16 15:19:07 +0100

    add funcs wind risk , precipitation risk

--- transformation/risk_score.py
+++ transformation/risk_score.py
@@ -16,3 +16,34 @@ class RiskCalculator:
 
         else:
             return 100
+        
+def precipitation_risk(self, precipitation):
+
+    if precipitation == 0:
+        return 0
+
+    elif precipitation < 5:
+        return 20
+
+    elif precipitation < 20:
+        return 50
+
+    elif precipitation < 50:
+        return 75
+
+    else:
+        return 100
+    
+def wind_risk(self, wind_speed):
+
+    if wind_speed < 20:
+        return 0
+
+    elif wind_speed < 40:
+        return 30
+
+    elif wind_speed < 60:
+        return 70
+
+    else:
+        return 100
